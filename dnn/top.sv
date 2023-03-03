3dd1095a54a137c9f3989da723403897d06965f9
Tanmay Anand <tanand5@tux-152.cae.wisc.edu>
2023-03-03 15:13:03 -0600

    out_ready change

--- dnn/top.sv
+++ dnn/top.sv
@@ -1,12 +1,12 @@
 module top (x0, x1, x2, x3, w04, w05, w06, w07, w14, w15, w16, w17, w24, w25, w26, w27, w34, w35, w36, 
-w37, w48, w58, w49, w59, w68, w69, w78, w79, out0, out1, in_ready, out10_ready, out11_ready, clk); 
+w37, w48, w58, w49, w59, w68, w69, w78, w79, out0, out1, in_ready, out0_ready, out1_ready, clk); 
  
 input [4:0] x0, x1, x2, x3, w04, w05, w06, w07, w14, w15, w16, w17, w24, w25, w26, w27, w34, w35, w36, w37, 
 w48, w58, w49, w59, w68, w69, w78, w79; 
 input in_ready; 
 input clk; 
 output [16:0] out0, out1; 
-output out10_ready, out11_ready; 
+output out0_ready, out1_ready; 
  
 // Implementation of the neural network 
 
@@ -133,7 +133,7 @@ always@(posedge clk) begin
 	end
 end
 
-assign out10_ready = stage6;
-assign out11_ready = stage6;
+assign out0_ready = stage6;
+assign out1_ready = stage6;
 
 endmodule 
